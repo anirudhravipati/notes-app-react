fde960ce30288159bbc3ca223e297c98a376d6dc
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2022-12-21 13:07:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// 1. import `ChakraProvider` component
+import './App.css'
+
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <div>
+      <h1> Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
